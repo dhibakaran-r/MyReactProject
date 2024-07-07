a6e4a957c313ae34b98d04193ac44ba4d92d490d
Dhibakaran R <151421565+dhibakaran-r@users.noreply.github.com>
2024-07-07 13:29:31 +0530

    Delete Components directory

--- Components/regexValidator.jsx
+++
@@ -1,5 +0,0 @@
-export const passwordValidator = password =>{
-    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
-    return passwordRegex.test(password)
-
-}
